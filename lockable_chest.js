3e9db77fb32e874e16948d94cb6b9a506d620570
Green <97376220+Nga2710@users.noreply.github.com>
2022-08-29 10:05:58 +0700

    Add files via upload

--- lockable_chest.js
+++ lockable_chest.js
@@ -4,7 +4,10 @@ mc.world.events.beforeItemUseOn.subscribe(data => {
     let dim = data.source.dimension, lc = data.blockLocation,
     iic = dim.getBlock(lc).getComponent("inventory").container.getItem(0), key = "minecraft:tripwire_hook";
     if (iic.id == key && data.item.id == key && iic.nameTag == data.item.nameTag) {} else {
-      data.source.runCommand(`playsound random.chestclosed @a ${lc.x} ${lc.y} ${lc.z}`); data.cancel = true;
+      data.cancel = true;
+      const sound = new mc.SoundOptions();
+      sound.location = new mc.Location(lc.x, lc.y, lc.z);
+      mc.world.playSound('random.chestclosed', sound)
     }
   }
   catch {}
